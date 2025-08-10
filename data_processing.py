e0c92f0913d2a1cf34507e8b460fb644f558189d
null <thenewgamerpro76@gmail.com>
2025-08-10 16:54:12 +0100

    added standardisation and normalisation for features in samples dataset

--- data_processing.py
+++ data_processing.py
@@ -8,11 +8,24 @@ dataset_folders = ["mit-bih", "training2017/training2017"]
 records = []
 labels = []
 physionet_label_map = {'N': "Normal", 'A': "AFib", 'O': "Other Arrythmia"}
+duration = 10 #seconds
 
 X = []
 y = []
 
-def load_physionet_dataset(physionet_path, reference_file_path):
+def standardise_dataset(X):
+    X_scaled = np.array([(x - np.mean(x)) / np.std(x) for x in X])
+    return X_scaled
+
+from sklearn.preprocessing import MinMaxScaler
+
+def normalise_dataset(X):
+    # Reshape X to 2D if needed (n_samples, n_features)
+    scaler = MinMaxScaler()
+    X_norm = scaler.fit_transform(X)
+    return X_norm
+
+def load_physionet_dataset(physionet_path, reference_file_path, X, y):
 
     reference_df = pd.read_csv(reference_file_path, header=None, names=['record', 'label'])
 
@@ -33,20 +46,29 @@ def load_physionet_dataset(physionet_path, reference_file_path):
 
     # entering it in dataset
     for paths in records:
-        record = wfdb.rdrecord(physionet_path+"\\"+paths)
+        record = wfdb.rdrecord(physionet_path + "\\" + paths)
+        fs = record.fs
+        samples = fs * duration
         ecg_signal = record.p_signal[:, 0]
-        X.append(ecg_signal)
+        trunc_ecg = ecg_signal[:samples]
+        # Pad if shorter than 'samples'
+        if len(trunc_ecg) < samples:
+            trunc_ecg = np.pad(trunc_ecg, (0, samples - len(trunc_ecg)), 'constant')
+        X.append(trunc_ecg)
 
     for label in labels:
         y.append(label)
 
-""" def load_mitbih_dataset(mitbih_path):
-    mitbih_records = []
+    X = np.array(X, dtype=np.float32)
+    y = np.array(y)
+    return X, y
 
-    #loading data
-    for file in os.listdir(mitbih_path):
-        if file.endswith(".hea"):
-            record_name = file[:-4]
-            mitbih_records.append(record_name) """
+def process_physionet_dataset(physionet_path, reference_file_path, X, y): #X and y should be empty lists
 
-    
+    X, y = load_physionet_dataset(physionet_path, reference_file_path, X, y)
+    X = standardise_dataset(X)
+    X = normalise_dataset(X)
+    return X, y
+
+X, y = process_physionet_dataset("training2017/training2017", "training2017/training2017/REFERENCE.csv", X, y)
+
