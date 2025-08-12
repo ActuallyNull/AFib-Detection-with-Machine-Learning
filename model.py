ac35ccb46cfd0e5034ec8a53ed2e3934ab55b5f3
null <thenewgamerpro76@gmail.com>
2025-08-12 02:23:56 +0100

    Model V.1.2, Spreadsheet updated, changed model architecture, added roc/auc curves, batch_size 64->100

--- model.py
+++ model.py
@@ -3,6 +3,9 @@ from keras import layers
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 from data_preperation import create_train_val_test_splits
 from sklearn.metrics import confusion_matrix, classification_report
+import matplotlib.pyplot as plt
+from sklearn.metrics import RocCurveDisplay
+from sklearn.preprocessing import label_binarize
 
 X_train, y_train, X_val, y_val, X_test, y_test = create_train_val_test_splits()
 
@@ -11,28 +14,12 @@ callback = [
     ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=1e-6, verbose=1)
 ]
 
-
 model = tf.keras.Sequential([
-    layers.Conv1D(32, kernel_size=5),
-    layers.Conv1D(32, kernel_size=5),
-    layers.MaxPool1D(pool_size=2),
-    layers.Conv1D(64, kernel_size=5),
-    layers.Conv1D(64, kernel_size=5),
-    layers.MaxPool1D(pool_size=2),
-    layers.Conv1D(128, kernel_size=5),
-    layers.Conv1D(128, kernel_size=5),
-    layers.Conv1D(128, kernel_size=5),
-    layers.MaxPool1D(pool_size=2),
-    layers.Conv1D(256, kernel_size=5),        
-    layers.Conv1D(256, kernel_size=5),        
-    layers.Conv1D(256, kernel_size=5),        
-    layers.MaxPool1D(pool_size=2),
-    layers.Conv1D(256, kernel_size=5),        
-    layers.Conv1D(256, kernel_size=5),        
-    layers.Conv1D(256, kernel_size=5),        
-    layers.MaxPool1D(pool_size=2),
+    layers.Conv1D(60, 1),
+    layers.Conv1D(50, 1),
+    layers.Conv1D(40, 1),
     layers.GlobalAveragePooling1D(),
-    layers.Dense(128, activation='relu'),
+    layers.Dense(20, activation='relu'),
     layers.Dense(3, activation='softmax')
 ])
 
@@ -43,7 +30,7 @@ model.compile(optimizer='adam',
 model.fit(X_train, 
           y_train, 
           epochs=100, 
-          batch_size=64,
+          batch_size=100,
           callbacks=callback,
           )
 
@@ -60,3 +47,15 @@ print(confusion_matrix(y_test, y_pred_classes))
 
 print("Classification Report:")
 print(classification_report(y_test, y_pred_classes))
+
+y_test_binary = label_binarize(y_test, classes=[0,1,2])
+
+for i in range(y_pred.shape[1]):
+    RocCurveDisplay.from_predictions(y_test_binary[:,i], y_pred[:,i], name=f"Class {i}")
+
+plt.plot([0,1], [0,1], 'k--')
+plt.xlabel("FPR (False Positive Rate)")
+plt.ylabel("TPR (True Positive Rate)")
+plt.title("OvR ROC Curve")
+plt.legend()
+plt.show()
