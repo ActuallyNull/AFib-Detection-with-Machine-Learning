cf0c908fe1826105eb43eb2a3c966b2413bcc846
null <thenewgamerpro76@gmail.com>
2025-09-18 19:22:01 -0400

    lets get this checked out

--- backend/main.py
+++ backend/main.py
@@ -56,7 +56,7 @@ TARGET_FS = 300
 TARGET_DURATION_SEC = 10
 TARGET_SAMPLES = TARGET_FS * TARGET_DURATION_SEC
 
-SHOWCASE_ECG_BASE_DIR = "../showcase_ecgs"
+SHOWCASE_ECG_BASE_DIR = "showcase_ecgs"
 PREDICTION_SHOWCASE_DIR = os.path.join(SHOWCASE_ECG_BASE_DIR, "prediction_showcase")
 VIEWER_SHOWCASE_DIR = os.path.join(SHOWCASE_ECG_BASE_DIR, "viewer_showcase")
 
@@ -98,9 +98,9 @@ def load_model():
     try:
         logger.info("Loading ECG classification model...")
         model = keras.models.load_model(
-            '../model.keras',
+            'model.keras',
             custom_objects={'SparseCategoricalFocalLoss': SparseCategoricalFocalLoss}
-        )
+                )
         model_loaded = True
         logger.info("Model loaded successfully")
         return True
